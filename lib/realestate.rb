20045370bddd86d920948a5f3975b3e48eccebdc
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-17 03:34:37 +0000

    commiting lib file

--- lib/realestate.rb
+++ lib/realestate.rb
@@ -1,5 +1,13 @@
-require "realestate/version"
-
+require_relative "./realestate/version"
+require_relative "./realestate/cli"
+require 'open-uri'
+require 'nokogiri'
 module Realestate
+
   # Your code goes here...
+end
+class RealEstate
+  html = open('http://www.google.com')
+  doc = Nokogiri::HTML(html)
+  doc.css
 end
