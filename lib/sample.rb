3ea6e352b7d25d7588c83bb40453be1edf0bf158
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-13 00:25:26 +0000

    deleted sample files

--- lib/sample.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
-state= "California"
-html = open('https://www.trulia.com/home_prices/' + state + '/',"User-Agent" => "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0")#no conversion of nil into string
-doc = Nokogiri::HTML(html)
-
-puts doc.css("td")[4]
