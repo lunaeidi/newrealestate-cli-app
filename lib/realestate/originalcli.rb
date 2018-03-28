a88ceb3642936e6b53191a92408f58ce540f1ff9
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-28 17:57:38 +0000

    reactored to accept user typos

--- lib/realestate/originalcli.rb
+++ lib/realestate/originalcli.rb
@@ -5,17 +5,6 @@ class CLI
     def greeting
       puts "Hello! Welcome to the RealEstate App!"
     end
-    def restart_or_exit
-    puts "Enter 'restart' to go back to main menu, or enter 'exit'" #if exit, goes to bottom
-    input5= gets.strip
-    if input5== 'restart'
-      call
-    elsif input5== 'exit'
-    else
-      restart_or_exit
-    end
-    end
-
     def call
     puts "Enter the name of the state for the Average Listing Price per county."
     state= gets.strip.capitalize
@@ -32,30 +21,29 @@ class CLI
       if input2== "all"
       stateinstance.counties.each do |county|#match the state the person searched.
       puts "#{county.name}: #{county.price}"
-        end
-    restart_or_exit
-      # puts "Enter 'restart' to go back to main menu, or enter 'exit'" #if exit, goes to bottom
-      # input5= gets.strip
-      # if input5== 'restart' || input5 != 'exit'
-      #   call
-      # elsif input5== 'exit'
-      #
-      # end
+    end
+
+      puts "Enter 'restart' to go back to main menu, or enter 'exit'" #if exit, goes to bottom
+      input5= gets.strip
+      if input5== 'restart' || input5 != 'exit'
+        call
+      elsif input5== 'exit'
+
+      end
     elsif input2== "one"
       puts "Enter the county name:"
       county= gets.strip.capitalize
 
       countyinstnace=stateinstance.counties.find{|c| c.name= county}
       puts countyinstnace.price
-      restart_or_exit
-      # puts "Please enter 'restart' to make another search, or enter 'exit'"
-      # input4= gets.strip
-      # if input4== 'restart'  #|| input4 != 'exit'
-      #   call
-      # elsif input4== 'exit'
-      # else
-      #
-      #   end
+      puts "Please enter 'restart' to make another search, or enter 'exit'"
+      input4= gets.strip
+      if input4== 'restart'  #|| input4 != 'exit'
+        call
+      elsif input4== 'exit'
+      else
+
+        end
     else
       call
 
